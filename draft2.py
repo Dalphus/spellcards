f6235b5f1612cb965f5bd85cf9a2f177838c9244
Dalphus <moofus2000@gmail.com>
2019-02-19 17:33:38 -0500

    draft2 finished
    on to draft3!
    also testing different fonts

--- draft2.py
+++ draft2.py
@@ -14,83 +14,98 @@ class CardInfo:
             x = line.find(':')
             self.info[line[:x]] = line[x+1:-1]
         f.close()
-        
+
 class Layout:
     def __init__(self):
         self.elements = [[]]
-        maxLevel = 0
-    def add(self,level,thing,mode,space=(1,1)):
-        if level > maxLevel:
-            maxLevel += 1
-            self.elements.append([(thing,mode,space)])
+    def add(self,level,thing,x='LEFT',y='TOP'):
+        if level >= len(self.elements):
+            self.elements.append([(thing,x,y)])
         else:
-            self.elements[level].append((thing,mode,space))
-    def draw(self):
-        padding = 30
-        size = (350,500)
-        color = (0,0,0)
-        x = pygame.Surface(size)
-        x.fill((255,255,255))
+            self.elements[level].append((thing,x,y))
+            
+    def draw(self,width=350):
+        height = [i[0][0].get_height() for i in self.elements]
+        height[4] += 30
+        height[7] += 30
+        
+        card = pygame.Surface((width,sum(height)))
+        card.fill((255,255,255))
 
-        for i in self.elements:
-            for j in i:
-                j.drawOn(x)
-
-        return x
+        for i in range(0,len(self.elements)):
+            for j in self.elements[i]:
+                x = y = 0
+                if j[1] == 'CENTER': x = (width-j[0].get_width())//2
+                elif j[1] == 'RIGHT': x = width-j[0].get_width()
+                if j[2] == 'CENTER': y = (height[i]-j[0].get_height())//2
+                elif j[2] == 'BOTTOM': y = height[i]-j[0].get_height()
+                card.blit(j[0],(x,sum(height[0:i])+y))
+        return card
+    
 class Draw:
     @classmethod
+    def getLevel(cls,school,level,font,d=80):
+        a = pygame.image.load("schools.png").convert_alpha()
+        a = pygame.transform.scale(a,(d*4,d*2))
+        b = pygame.Surface((d,d))
+        b.fill((255,255,255))
+        b.blit(a,(0,0),(d*2,d,d,d))
+        c = font.render(level,1,(0,0,0))
+        b.blit(c,((d-c.get_width())//2,(d-c.get_height())//2))
+        return b
+    @classmethod
+    def getRC(cls,r,c,d=60):#do this without images
+        b = pygame.Surface((d*2,d))
+        b.fill((255,255,255))
+        b.set_colorkey((255,255,255))
+        a = pygame.image.load('ritual.png').convert_alpha()
+        a = pygame.transform.scale(a,(d,d))
+        b.blit(a,(0,0))
+        a = pygame.image.load('concentration.png').convert_alpha()
+        a = pygame.transform.scale(a,(d,d))
+        b.blit(a,(d,0))
+        return b
+    @classmethod
+    def getComponents(cls,c,d=60):
+        V = pygame.image.load('visual.png').convert_alpha()
+        V = pygame.transform.scale(V,(d,d))
+        S = pygame.image.load('sematic.png').convert_alpha()
+        S = pygame.transform.scale(S,(d,d))
+        M = pygame.image.load('material.png').convert_alpha()
+        M = pygame.transform.scale(M,(d,d))
+        b = pygame.Surface((3*d,d))
+        b.fill((255,255,255))
+        b.set_colorkey((255,255,255))
+        b.blit(V,(0,0));b.blit(S,(d,0));b.blit(M,(d*2,0))
+        return b
+    
+    @classmethod
     def visualize(cls,info):
-        
         font_info = [('Calibri',17),('Calibri',73),('Calibri',26)]
         fonts = []
         for i in font_info:
             fonts.append(pygame.font.SysFont(i[0],i[1]))
         
-        color = (0,0,0)
-        size = (350,500)
-        
-        a = cls.wrap(info['description'],size[0]-60,fonts[0])
-        b = cls.wrap(info['higher'],size[0]-60,fonts[0])
+        width = 350
 
-        #surface
-        card_surface = pygame.Surface(size)
-        card_surface.fill((255,255,255))
-        #border
-        pygame.draw.rect(card_surface,color,pygame.Rect(0,0,size[0],size[1]),25)
-        #level
-        pygame.draw.circle(card_surface,(255,255,255),(35,35),40)
-        pygame.draw.circle(card_surface,color,(35,35),45,12)
-        c = fonts[2].render(info['level'],1,(0,0,0))
-        card_surface.blit(c,(35-c.get_width()/2,35-c.get_height()/2))
-        #name
-        c = fonts[0].render(info['name'],1,(0,0,0))
-        card_surface.blit(c,(size[0]/2-c.get_width()/2,30))
-        #PHB
-        c = fonts[0].render('PHB '+info['page'],1,(0,0,0))
-        card_surface.blit(c,(size[0]-30-c.get_width(),30))
-        #casting times
-        c = fonts[0].render('Casting time: '+info['casting-time'],1,(0,0,0))
-        card_surface.blit(c,(30,55))
-        #range
-        c = fonts[0].render('Range: '+info['range'],1,(0,0,0))
-        card_surface.blit(c,(30,75))
-        #duration
-        c = fonts[0].render('Duration: '+info['duration'],1,(0,0,0))
-        card_surface.blit(c,(30,95))
-        #damage
-        c = fonts[1].render(info['damage'],1,(0,0,0))
-        card_surface.blit(c,(30,180))
-        #type
-        c = fonts[2].render(info['type'],1,(0,0,0))
-        card_surface.blit(c,(190,215))
-
-        card_surface.blit(a,(30,260))
-        card_surface.blit(b,(30,320))
+        l = Layout()
+        l.add(0,cls.getLevel(info['school'],info['level'],fonts[2]))
+        l.add(0,cls.getText(info['name'],fonts[0]),'RIGHT')#idk, bigger and local center maybe
+        l.add(1,cls.getText('Casting Time: '+info['casting-time'],fonts[0]))
+        l.add(2,cls.getText('Range: '+info['range'],fonts[0]))
+        l.add(3,cls.getText('Duration: '+info['duration'],fonts[0]))
+        l.add(2,cls.getRC(info['ritual'],info['concentration']),'RIGHT','CENTER')
+        l.add(4,cls.getComponents(info['components']),'CENTER','CENTER')#divider
+        l.add(5,cls.getText(info['damage'],fonts[1]))
+        l.add(5,cls.getText(info['type'],fonts[2]),'RIGHT','BOTTOM')#flush
+        l.add(6,cls.wrap(info['description'],width,fonts[0]))
+        l.add(7,cls.wrap(info['higher'],width,fonts[0]),'LEFT','CENTER')#bold
+        l.add(8,cls.getText('PHB '+info['page'],fonts[0]),'RIGHT')
         
-        return card_surface
+        return l.draw()
 
     @classmethod
-    def wrap(csl,text,width,font):
+    def wrap(cls,text,width,font):
         words = [i+' ' for i in text.split(' ')]
 
         line = words[0]
@@ -113,8 +128,8 @@ class Draw:
         return text_surface
 
     @classmethod
-    def outline(rect):
-        pass
+    def getText(cls,text,font):
+        return font.render(text,1,(0,0,0))
     
 class __main__:
 
